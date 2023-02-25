bee1f5c32f347da2f18e83879fa1e27251214651
gizem <gizemtuncer17@gmail.com>
2023-02-25 14:12:20 +0300

    created contact.html file

--- blog/views/contact.py
+++ blog/views/contact.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 #* __init__.py sayfasına dahil etmeyi unutma!
 def contact(request):
-    return render(request , 'pages/home.html', context={})
+    return render(request , 'pages/contact.html', context={})
