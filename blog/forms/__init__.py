0fcd68a377cbd9c31f57c2157fd8d1e5fc6900fb
gizem <gizemtuncer17@gmail.com>
2023-03-03 14:22:18 +0300

    created addComment method

--- blog/forms/__init__.py
+++ blog/forms/__init__.py
@@ -1,5 +1,6 @@
 from .contact import contactForm
 from .textAdd import textAddForm
 from .textUpdate import textUpdateForm
+from .commentAdd import commentAddForm
 
 
--- blog/views/detail.py
+++ blog/views/detail.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import textModel
-
+from blog.forms import commentAddForm
 def detail(request,slug):
     text = get_object_or_404(textModel, slug=slug)
     print(text)
     comments = text.commentS.all()
+    
+    if request.method == 'POST':
+        addComment = commentAddForm(data=request.POST)
+        if addComment.is_valid():
+            comment = addComment.save(commit=False)
+            comment.author =request.user
+            comment.text = text
+            comment.save()       
+        
+    addComment = commentAddForm()
+    
+    
     return render(request,'pages/detail.html', context={
         'text': text,
-        'comments': comments
+        'comments': comments,
+        'addComment': addComment,
     })
     
     
